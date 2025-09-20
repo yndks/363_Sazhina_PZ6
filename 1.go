bd0a4d8217a77aca18317cdad41847e8f77f113a
yndks <karina_top01@mail.ru>
2025-09-20 08:52:52 +0500

    Create 1.go

---
+++ 1.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+ "fmt"
+ "sync"
+)
+
+func main() {
+ var counter int
+ var mu sync.Mutex
+ var wg sync.WaitGroup
+
+ for i := 0; i < 10; i++ {
+  wg.Add(1)
+  go func() {
+   defer wg.Done()
+   for j := 0; j < 1000; j++ {
+    mu.Lock()
+    counter++
+    mu.Unlock()
+   }
+  }()
+ }
+
+ wg.Wait()
+ fmt.Println("Количество просмотров:", counter)
+}
